ca027bd3078b7b2207e572ed45150f9492ccaa90
01Clarian <clarionnorth@gmail.com>
2019-11-21 16:10:21 -0500

    cart reducer items and cart dropdown component additions

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,10 @@
+// adds a quantity value to our shop list 
+// check to find when cart item current is equal to add
+// only when they're equal add quantity value to the ones that are equal 
+//otherwise just return all the card items
+//  and if there are no matches cartitems id to next ones return all cartitems, 
+//ones to add and set quantity to 1
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id 
     === cartItemToAdd.id);
