103dd8976f349e7d63d86e86cc19f2db37c397be
01Clarian <clarionnorth@gmail.com>
2019-11-26 15:50:38 -0500

    removing psuh build from logger into production

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,13 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
 
-const middleware = [logger];
+const middleware = [];
+
+// switch logger from dev to production 
+// if dev is development push logger to array
+if(process.env.NODE_ENV === 'development') {
+    middleware.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware))
 export const persistor = persistStore(store);
